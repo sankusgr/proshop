0cef4b7d1c5d27148ffcc35821f923099d6ddfbf
Sanketh <Sanketh.T@kaplan.com>
2024-10-07 07:24:50 +0530

    Backend folder, express server setup

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,11 +1,20 @@
-import products from "../products";
+import { useEffect, useState } from "react";
+import axios from "axios";  
 import { Link, useParams } from "react-router-dom";
 import { Row, Col, Image, ListGroup, Button, Card } from "react-bootstrap";
 import Rating from "../components/Rating";
 
 const ProductScreen = () => {
+    const [product, setProduct] = useState([]);
     const { id: productId } = useParams();
-    const product = products.find(product => product._id === productId);
+
+    useEffect(() => {
+        const fetchProduct = async () => {
+            const { data } = await axios.get(`/api/product/${productId}`);
+            setProduct(data);
+        };
+        fetchProduct();
+    }, [productId]);
 
     return (
         <>

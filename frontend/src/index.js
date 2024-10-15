3b9fd04ffaafabf1c12f116945985d4d52a585a8
Sanketh <Sanketh.T@kaplan.com>
2024-10-15 08:18:31 +0530

    Setting up redux store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,6 +7,8 @@ import {
   Route,
   RouterProvider
 } from 'react-router-dom';
+import  { Provider } from 'react-redux';
+import store from './store';
 import './index.css';
 import App from './App';
 import HomeScreen from './screens/HomeScreen';
@@ -25,7 +27,9 @@ const router = createBrowserRouter(
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
 
--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,27 +1,25 @@
-import { useEffect, useState } from "react";
-import axios from "axios";  
 import { Link, useParams } from "react-router-dom";
 import { Row, Col, Image, ListGroup, Button, Card } from "react-bootstrap";
 import Rating from "../components/Rating";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
+import { useGetProductDetailsQuery } from "../slices/productsApiSlice";
 
 const ProductScreen = () => {
-    const [product, setProduct] = useState([]);
-    const { id: productId } = useParams();
-
-    useEffect(() => {
-        const fetchProduct = async () => {
-            const { data } = await axios.get(`/api/products/${productId}`);
-            setProduct(data);
-        };
-        fetchProduct();
-    }, [productId]);
+    const { id } = useParams();
+    const { data: product, isLoading, error } = useGetProductDetailsQuery(id);
 
     return (
         <>
         <Link className='btn btn-light my-3' to='/'>
             Go Back
         </Link>
-        <Row>
+        {isLoading ? (
+            <Loader />
+        ) : error ? (
+            <Message variant='danger'>{error?.data?.message || error.error}</Message>
+        ) : (
+            <Row>
             <Col md={5}>
                 <Image src={product.image} alt={product.name} fluid />
             </Col>
@@ -64,7 +62,9 @@ const ProductScreen = () => {
                     </ListGroup>
                 </Card>
             </Col>
-        </Row>            
+        </Row>
+        )}
+                    
       </>
     )
 };
